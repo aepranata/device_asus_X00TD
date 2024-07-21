092c0a31abd77883436a8aff621b28affaa88304
Bruno Martins <bgcngm@gmail.com>
2024-07-22 00:24:06 +0530

    X00TD: Use common libqti-perfd-client and power-libperfmgr
    Change-Id: I7cc2a5ec484b9e7e733131f41004a1646b661aa7

--- device.mk
+++ device.mk
@@ -406,15 +406,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0 \
-    android.hardware.power@1.0.vendor \
-    android.hardware.power@1.1 \
-    android.hardware.power@1.1.vendor \
-    android.hardware.power@1.2 \
-    android.hardware.power@1.2.vendor \
-    android.hardware.power@1.3 \
-    android.hardware.power@1.3.vendor \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
@@ -510,6 +503,12 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 QCOM_SOONG_NAMESPACE := \
     $(DEVICE_PATH)/qcom-caf
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
