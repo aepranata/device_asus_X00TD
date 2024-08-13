7a7de289da19512dbba8c48fba6dc4cd68c706f5
Jimmy Shiu <jimmyshiu@google.com>
2024-08-13 07:42:22 +0000

    X00TD: power-libperfmgr: HintManager: Handle ADPF tag profile with EventNode
    * For backward-compatible:
      - PowerSessionManager: Keep the old `updateHintMode()` flow for
        backward compatible.
      - (Old) HM.Get/SetAdpfProfile: Rename to
        Get/SetAdpfProfileFromDoHint().
      - Add TODO tag for future deprecation.
    
    * For new ADPF tag profile switching:
      - HM.DumpToFd(): Add new session tag profile dump info.
      - HM.ParseNodes(): Add EventNode parsing and construct the EventNode
        with HM.OnNodeUpdate() callback function to handle
        Adpf-Config-Changed-Event.
      - HM.GetFromJSON(): Build tag and profile association with a
        tag_profile_map_.
      - (New)HM.Get/SetAdpfProfile(): Operate tag_profile_map_
    
    Bug: 332301126
    Test: atest libperfmgr_test
    Change-Id: I7e8a23825c0945d0100c9c616cc2b297e34d18a3

--- power-libperfmgr/aidl/PowerSessionManager.cpp
+++ power-libperfmgr/aidl/PowerSessionManager.cpp
@@ -76,6 +76,7 @@ static void set_uclamp_min(int tid, int min) {
 }
 }  // namespace
 
+// TODO(jimmyshiu@): Deprecated. Remove once all powerhint.json up-to-date.
 void PowerSessionManager::updateHintMode(const std::string &mode, bool enabled) {
     ALOGV("PowerSessionManager::updateHintMode: mode: %s, enabled: %d", mode.c_str(), enabled);
     if (enabled && mode.compare(0, 8, "REFRESH_") == 0) {
@@ -87,8 +88,8 @@ void PowerSessionManager::updateHintMode(const std::string &mode, bool enabled) 
             mDisplayRefreshRate = 60;
         }
     }
-    if (HintManager::GetInstance()->GetAdpfProfile()) {
-        HintManager::GetInstance()->SetAdpfProfile(mode);
+    if (HintManager::GetInstance()->GetAdpfProfileFromDoHint()) {
+        HintManager::GetInstance()->SetAdpfProfileFromDoHint(mode);
     }
 }
 
