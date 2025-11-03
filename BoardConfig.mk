9b0855c764de098bea8bb6011b966d3c0eef89a9
Aditya Eka Pranata <aepranata@gmail.com>
2025-11-04 05:55:10 +0700

    Revert "X00TD: Change gesture and dt2w node to touchpanel"
    This reverts commit ddcf1222b522fb1e59e65152ed87950c0152f736.
    
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -233,7 +233,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 TARGET_BOARD_PLATFORM := sdm660
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
+TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
