ddcf1222b522fb1e59e65152ed87950c0152f736
Aditya Eka Pranata <aepranata@gmail.com>
2025-11-07 22:22:27 +0700

    X00TD: Change gesture and dt2w node to touchpanel
    * Inspired and taken from https://github.com/xtrymind/device_asus_X00T
    
    Co-authored-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Co-authored-by: SonicBSV <sonicbsv@gmail.com>
    Change-Id: Ib03200c6e7d90afbccb986d448232c6b34e88f38

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -228,7 +228,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 TARGET_BOARD_PLATFORM := sdm660
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
