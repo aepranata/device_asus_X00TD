0efeccd28cea31ee6228c8eb14ca4f585f1cdc90
Aditya Eka Pranata <aepranata@gmail.com>
2025-12-03 02:51:45 +0700

    X00TD: Update NFC configs
    Change-Id: If833b70048a5113a2ce7f6e73ccfeaba679a67d7

--- device.mk
+++ device.mk
@@ -391,24 +391,16 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/android.hardware.nfc.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/com.nxp.mifare.xml \
-    $(DEVICE_PATH)/configs/nfc/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/android.software.nfc.beam.xml
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/com.android.nfc_extras.xml
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(DEVICE_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service
-
-PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.2.vendor \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag
 
 # OEM Unlock reporting
@@ -544,6 +536,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor \
     libavservices_minijail.vendor \
     librmnetctl \
     libsqlite.vendor:64 \
