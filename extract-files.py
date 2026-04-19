7bfbc1859b52fa8259a9d840ceed30363dfae453
Aditya Eka Pranata <aepranata@gmail.com>
2026-04-20 00:38:47 +0700

    X00TD: Switch to python extract-utils
    Change-Id: Idfd8aa271dfc4f5a958fa75da07ba41547e2001b

---
+++ extract-files.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import os
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/asus/X00TD',
+    'hardware/qcom-caf/sdm660',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'com.qualcomm.qti.imscmservice@1.0',
+        'com.qualcomm.qti.imscmservice@2.0',
+        'com.qualcomm.qti.imscmservice@2.1',
+        'com.qualcomm.qti.imscmservice@2.2',
+        'com.qualcomm.qti.uceservice@2.0',
+        'com.qualcomm.qti.uceservice@2.1',
+        'com.qualcomm.qti.uceservice@2.2',
+        'com.qualcomm.qti.uceservice@2.3',
+        'vendor.qti.data.factory@2.0',
+        'vendor.qti.data.factory@2.1',
+        'vendor.qti.data.factory@2.2',
+        'vendor.qti.data.factory@2.3',
+        'vendor.qti.data.mwqem@1.0',
+        'vendor.qti.data.slm@1.0',
+        'vendor.qti.hardware.alarm@1.0',
+        'vendor.qti.hardware.data.cne.internal.api@1.0',
+        'vendor.qti.hardware.data.cne.internal.constants@1.0',
+        'vendor.qti.hardware.data.cne.internal.server@1.0',
+        'vendor.qti.hardware.data.connection@1.0',
+        'vendor.qti.hardware.data.connection@1.1',
+        'vendor.qti.hardware.data.dynamicdds@1.0',
+        'vendor.qti.hardware.data.dynamicdds@1.1',
+        'vendor.qti.hardware.data.flow@1.0',
+        'vendor.qti.hardware.data.flow@1.1',
+        'vendor.qti.hardware.data.iwlan@1.0',
+        'vendor.qti.hardware.data.latency@1.0',
+        'vendor.qti.hardware.data.lce@1.0',
+        'vendor.qti.hardware.data.qmi@1.0',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.mwqemadapter@1.0',
+        'vendor.qti.hardware.radio.am@1.0',
+        'vendor.qti.hardware.radio.ims@1.0',
+        'vendor.qti.hardware.radio.ims@1.1',
+        'vendor.qti.hardware.radio.ims@1.2',
+        'vendor.qti.hardware.radio.ims@1.3',
+        'vendor.qti.hardware.radio.ims@1.4',
+        'vendor.qti.hardware.radio.ims@1.5',
+        'vendor.qti.hardware.radio.ims@1.6',
+        'vendor.qti.hardware.radio.ims@1.7',
+        'vendor.qti.hardware.radio.ims@1.8',
+        'vendor.qti.hardware.radio.internal.deviceinfo@1.0',
+        'vendor.qti.hardware.radio.lpa@1.0',
+        'vendor.qti.hardware.radio.qcrilhook@1.0',
+        'vendor.qti.hardware.radio.qtiradio@1.0',
+        'vendor.qti.hardware.radio.qtiradio@2.0',
+        'vendor.qti.hardware.radio.qtiradio@2.1',
+        'vendor.qti.hardware.radio.qtiradio@2.2',
+        'vendor.qti.hardware.radio.qtiradio@2.3',
+        'vendor.qti.hardware.radio.qtiradio@2.4',
+        'vendor.qti.hardware.radio.uim@1.0',
+        'vendor.qti.hardware.radio.uim@1.1',
+        'vendor.qti.hardware.radio.uim@1.2',
+        'vendor.qti.hardware.radio.uim_remote_client@1.0',
+        'vendor.qti.hardware.radio.uim_remote_client@1.1',
+        'vendor.qti.hardware.radio.uim_remote_client@1.2',
+        'vendor.qti.hardware.radio.uim_remote_server@1.0',
+        'vendor.qti.hardware.slmadapter@1.0',
+        'vendor.qti.ims.callcapability@1.0',
+        'vendor.qti.ims.callinfo@1.0',
+        'vendor.qti.ims.factory@1.0',
+        'vendor.qti.ims.factory@1.1',
+        'vendor.qti.ims.rcsconfig@1.0',
+        'vendor.qti.ims.rcsconfig@1.1',
+        'vendor.qti.ims.rcsconfig@2.0',
+        'vendor.qti.ims.rcsconfig@2.1',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.latency@2.0',
+        'vendor.qti.latency@2.1',
+        'vendor.qti.latency@2.2',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libmmosal',
+    ): lib_fixup_vendor_suffix,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    # Camera - uneeded
+    'vendor/lib/libmmcamera_tuning.so': blob_fixup()
+        .remove_needed('libmm-qcamera.so'),
+    # Fingerprint - uneeded
+    'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-lite.so', 'libprotobuf-cpp-lite-21.12.so'),
+    'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so': blob_fixup()
+        .replace_needed('libhidlbase.so.so', 'libhidlbase-v32.so'),
+    'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so': blob_fixup()
+        .replace_needed('libhidlbase.so.so', 'libhidlbase-v32.so'),
+    # Fingerprint - liblog dep.
+    'vendor/lib64/hw/cdfinger.fingerprint.default.so': blob_fixup()
+        .add_needed('liblog.so'),
+    # Fingerprint - so name fixups
+    ('vendor/lib64/hw/cdfinger.fingerprint.default.so', 'vendor/lib64/hw/fingerprint.sdm660.so'):blob_fixup()
+        .fix_soname(),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'X00TD',
+    'asus',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
