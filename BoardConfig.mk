2fbd9f4ea9d0d860c0167cefa4476eaf5ed92333
Aditya Eka Pranata <aepranata@gmail.com>
2026-06-01 11:36:15 +0700

    fixup! X00TD: Prepare BoardConfig for Android 16 bringup
    Change-Id: Ie153e5ac990f2d078418b7d8fec6aabd0bb9027f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -260,7 +260,6 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
