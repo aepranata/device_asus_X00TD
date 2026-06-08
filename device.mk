6454e21da8eca677914fa87bed47032b86bbf7d5
Aditya Eka Pranata <aepranata@gmail.com>
2026-06-09 05:37:33 +0700

    X00TD: camera: mk -> bp
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I3389ee3add39c8f8d8f61472a30283b94a495bb2

--- device.mk
+++ device.mk
@@ -93,20 +93,14 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.sdm660
-
-PRODUCT_PACKAGES += \
-    android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
+    android.hardware.camera.provider-service_32.lineage \
+    camera.sdm660:32 \
+    libgui_vendor \
     liblz4.vendor \
-    libutilscallstack.vendor \
     libpng.vendor:32 \
-    libxml2
+    libutilscallstack.vendor \
+    libxml2 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
